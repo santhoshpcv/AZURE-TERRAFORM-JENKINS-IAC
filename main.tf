04b154f8e71c73fc7c506491726f4b43420f56c7
santhoshpcv <66933280+santhoshpcv@users.noreply.github.com>
2021-06-01 15:58:11 +0530

    Update main.tf
    zrs

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "azurerm_storage_account" "san1" {
   resource_group_name      = azurerm_resource_group.rg1.name
   location                 = azurerm_resource_group.rg1.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "ZRS"
 
   tags = {
     environment = "stage"
