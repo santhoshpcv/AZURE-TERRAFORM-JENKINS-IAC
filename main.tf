afdf68a439ba762600f5e1c6fc05636693d2badc
santhoshpcv <66933280+santhoshpcv@users.noreply.github.com>
2021-06-01 16:06:16 +0530

    Update main.tf
    fix

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "azurerm_storage_account" "san1" {
   resource_group_name      = azurerm_resource_group.rg1.name
   location                 = azurerm_resource_group.rg1.location
   account_tier             = "Standard"
-  account_replication_type = "ZRS"
+  account_replication_type = "LRS"
 
   tags = {
     environment = "stage"
