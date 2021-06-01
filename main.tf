4ed2ecbf0a29eb26dd47759628adac6e5435f25d
santhoshpcv <66933280+santhoshpcv@users.noreply.github.com>
2021-06-01 14:32:41 +0530

    Update main.tf
    storage

--- main.tf
+++ main.tf
@@ -24,17 +24,17 @@ resource "azurerm_resource_group" "rg1" {
   location = "West Europe"
 }
 
-# resource "azurerm_storage_account" "san1" {
-#   name                     = "santhoshteststorage"
-#   resource_group_name      = azurerm_resource_group.rg1.name
-#   location                 = azurerm_resource_group.rg1.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
+resource "azurerm_storage_account" "san1" {
+  name                     = "santhoshteststorage"
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = azurerm_resource_group.rg1.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-#   tags = {
-#     environment = "staging"
-#   }
-# }
+  tags = {
+    environment = "staging"
+   }
+ }
 
 
 # resource "azurerm_virtual_network" "vnet1" {
